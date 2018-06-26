6ce47e2f67a77a90886df2748ccfbd2283e8210f
nanfengpo <lsxxxxxx@126.com>
2018-06-26 16:41:15 +0800

    删除书签

--- Automatic upgrade/users/adminx.py
+++ Automatic upgrade/users/adminx.py
@@ -10,22 +10,11 @@ class BaseSetting(object):
     show_bookmarks = False
 
 class GlobalSettings(object):
+    show_bookmarks = False
     site_title = '自动升级后台'
     site_footer = '拓叭吧'
     menu_style = 'accordion'#下拉菜单
 
-# class EmailVerifyRecordAdmin(object):
-#     list_display = ['code','email','send_type','send_time']
-#     search_fields = ['code','email','send_type']
-#     list_filter = ['code','email','send_type','send_time']
-#
-# class BannerAdmin(object):
-#
-#     list_display = ['title','image','url','index','add_time']
-#     search_fields = ['title','image','url','index']
-#     list_filter = ['title','image','url','index','add_time']
 
-# xadmin.site.register(EmailVerifyRecord,EmailVerifyRecordAdmin)
-# xadmin.site.register(Banner,BannerAdmin)
 xadmin.site.register(views.BaseAdminView,BaseSetting)
 xadmin.site.register(views.CommAdminView,GlobalSettings)
--- Automatic upgrade/users/models.py
+++ Automatic upgrade/users/models.py
@@ -18,31 +18,3 @@ class UserProfile(AbstractUser):
 
     def __str__(self):
         return self.username
-#
-# class EmailVerifyRecord(models.Model):
-#     code = models.CharField(max_length=20,verbose_name=u'验证码')
-#     email = models.EmailField(max_length=50,verbose_name=u'邮箱')
-#     send_type = models.CharField(max_length=20,choices=(('register',u'注册'),('forget',u'忘记密码')))
-#     send_time = models.DateTimeField(default=datetime.now)
-#
-#     class Meta:
-#         verbose_name = u'邮箱验证'
-#         verbose_name_plural = verbose_name
-#
-#     def  __str__(self):
-#
-#         return '{0}({1})'.format(self.code,self.email)
-#
-# class Banner(models.Model):
-#     title = models.CharField(max_length=100,verbose_name=u'标题')
-#     image = models.ImageField(max_length=200,upload_to='banner/%y/%m',verbose_name=u'轮播')
-#     url = models.URLField(max_length=200,verbose_name=u'访问地址')
-#     index = models.IntegerField(default=100,verbose_name=u'顺序')
-#     add_time = models.DateTimeField(default=datetime.now,verbose_name=u'添加日期')
-#
-#     class Meta:
-#         verbose_name = u'轮播图'
-#         verbose_name_plural = verbose_name
-#
-#     def __str__(self):
-#         return self.title
