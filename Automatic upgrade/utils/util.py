a3734386c07ffa76c1b5ea150fa4c72b6246e792
LOVE-Yourself <1173196683@qq.com>
2018-06-27 14:10:46 +0800

    test_stash

--- Automatic upgrade/utils/util.py
+++ Automatic upgrade/utils/util.py
@@ -83,6 +83,7 @@ class compareManager:
         while True:
             buffer = fp.read(8192)
             if not buffer: break
+            print('---->更改文件gitstash隐藏')
             checksum.update(buffer.encode('utf8'))
         fp.close()
         checksum = checksum.digest()
